7f44fbb35bccbe9e39b41205bccc2214a7608680
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2023-11-26 18:12:16 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (34.33%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int numDecodings(string s) {
-
-        function<int(int i)> func;
         int n = s.size();
-        vector<int> memo(n, -1);
+        vector<int> dp(n + 1);
+        dp[n] = 1;
+        dp[n - 1] = (s[n - 1] == '0') ? 0 : 1;
+        
+        for (int i = n - 2; i >= 0; i--) {
+            if (s[i] == '0') {
+                dp[i] = 0;
+                continue;
+            }  
+            dp[i] = dp[i + 1];
 
-        func = [&] (int i) {
-            if (i >= n) return 1;
-            if (s[i] == '0') return 0;
-            if (i == n - 1) return 1;
+            if (((s[i] - '0') * 10 + s[i + 1] - '0') <= 26) 
+                dp[i] += dp[i + 2];
+        }
 
-            if (memo[i] != -1) return memo[i];
-
-            int res = func(i + 1);
-            if ( i < n - 1 && ((s[i] - '0') * 10 + s[i + 1] - '0') < 27)
-                res += func(i + 2);
-
-            return memo[i] = res;
-        };
-
-        return func(0);
+        return dp[0];
     }
 };
