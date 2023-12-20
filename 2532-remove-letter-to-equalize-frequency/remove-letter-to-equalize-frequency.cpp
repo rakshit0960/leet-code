6e4ac9bcb79a214f7a2122b5bf80178a6a51e6bc
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2023-12-20 10:00:02 +0530

    Time: 3 ms (39.54%) | Memory: 6.5 MB (69.77%) - LeetSync

--- 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.cpp
+++ 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     bool equalFrequency(string word) {
-        map<char, int>mp;
-        int n = word.size();
-        for(int i= 0; i<n; i++){
-            mp[word[i]]++;
+        unordered_map<char, int> freq;
+        for (char ch : word) freq[ch]++;
+
+        int maxFreq = INT_MIN, minFreq = INT_MAX;
+        for (auto i : freq) {
+            maxFreq = max(maxFreq, i.second);
+            minFreq = min(minFreq, i.second);
         }
-        int mn = INT_MAX;
-        int mx = INT_MIN;
-        for(auto it: mp){
-            mn = min(mn, it.second);
-            mx = max(mx, it.second);
-        }
-        if(mp.size() == 1)
-            return 1;
-        if(mp.size()* mn+1 == word.size())
-            return 1;
-        if(mx* (mp.size()-1) +1 == word.size() && mn == 1)
-            return 1;
-        return 0;
-        
+
+        if (freq.size() == 1 || 
+        freq.size() == word.size() 
+        || (minFreq == 1 && maxFreq * (freq.size() - 1) == word.size() - 1)
+        || maxFreq - minFreq == 1 && minFreq * (freq.size() - 1) == word.size() - maxFreq) return true;
+
+        else return false;
     }
 };
