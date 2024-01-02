79e46fc678ca0a52b5c34bc7bd930c0d4010b3b9
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-01-03 00:23:52 +0530

    Time: 223 ms (5.09%) | Memory: 61.1 MB (12.77%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,39 +1,12 @@
 class Solution {
 public:
-    void heapify(vector<int>& nums, int i, int size) {
-        int right = i * 2 + 1;
-        int left = i * 2 + 2;
-
-        int largest = i;
-
-        if (right < size && nums[largest] < nums[right])
-            largest = right;
-        
-        if (left < size && nums[largest] < nums[left])
-            largest = left;
-
-        swap(nums[largest], nums[i]);
-
-        if (largest != i) heapify(nums, largest, size);
-    }
-
-    void buildHeap(vector<int>& nums, int size) {
-        for (int i = (size - 2) / 2; i >= 0; i--)
-            heapify(nums, i, size);
-    }
-
-    int pop(vector<int>& nums, int& size) {
-        size--;
-        if (size >= 0) 
-            swap(nums[0], nums[size]);
-        heapify(nums, 0, size);
-        return nums[size];
-    }
-
     int findKthLargest(vector<int>& nums, int k) {
-        int size = nums.size();
-        buildHeap(nums, size);
-        for (int i = 1; i < k; i++) pop(nums, size);
-        return pop(nums, size);
+        priority_queue<int, vector<int>> maxHeap;
+
+        for (int i : nums) maxHeap.push(i);
+
+        for (int i = 1; i < k; i++) maxHeap.pop();
+
+        return maxHeap.top();
     }
 };
