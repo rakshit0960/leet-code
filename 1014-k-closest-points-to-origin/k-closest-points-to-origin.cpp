9171794590febee8b1e4147369948e287a74cd2f
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-01-02 22:46:21 +0530

    Time: 420 ms (15.63%) | Memory: 64.3 MB (74.41%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        typedef pair<int, vector<int>> queue_pair;
+        typedef pair<int, int> queue_pair;
         priority_queue<queue_pair, vector<queue_pair>, greater<queue_pair>> pq;
 
-        for (auto point : points) {
-            int x = point[0], y = point[1];
-            int distence = pow(x, 2) + pow(y, 2);
-            pq.push({distence, point});
+        for (int i = 0; i < points.size(); i++) {
+            int distence = pow(points[i][0], 2) + pow(points[i][1], 2);
+            pq.push({distence, i});
         }
 
         vector<vector<int>> res;
         for (int i = 0; i < k; i++) {
             auto top = pq.top();
             pq.pop();
-            res.push_back(top.second);
+            res.push_back(points[top.second]);
         }
 
         return res;
