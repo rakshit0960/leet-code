352e37f144f3352ee1b911e1ce0931fa56f20ae8
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-01-14 16:57:47 +0530

    Time: 3 ms (48.08%) | Memory: 7 MB (42.11%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -6,19 +6,19 @@ public:
 
         function<void(int, int)> comb;
         comb = [&] (int num, int sum) {
+            if (num < 0 || curr.size() > k) return;
             if (curr.size() == k) {
                 if (sum == n) res.push_back(curr);
                 return;
             } 
-            if (num > 9 || curr.size() > k) return;
 
-            comb(num + 1, sum);
+            comb(num - 1, sum);
             curr.push_back(num);
-            comb(num + 1, sum + num);
+            comb(num - 1, sum + num);
             curr.pop_back();
         }; 
 
-        comb(1, 0);
+        comb(9, 0);
         return res;
     }
 };
