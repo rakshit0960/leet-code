e19cc7fcabc62c1c74a5ba7604a6930873042599
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-03-25 23:07:49 +0530

    Time: 10 ms (34.71%) | Memory: 12.9 MB (37.98%) - LeetSync

--- 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        unordered_map<int, vector<int>> mp;
+        unordered_map<int, priority_queue<int, vector<int>, greater<int>>> mp;
         int rows = mat.size();
         int cols = mat[0].size();
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                mp[i - j].push_back(mat[i][j]);        
+                mp[i - j].push(mat[i][j]);        
             }
-        }
-
-        for (auto& it: mp) {
-            sort(it.second.begin(), it.second.end(), [](int a, int b) {return a > b;});
         }
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                auto& vect = mp[i - j];
-                mat[i][j] = vect[vect.size() - 1];
-                vect.pop_back();
+                mat[i][j] = mp[i - j].top();
+                mp[i - j].pop();
             }
         }
 
