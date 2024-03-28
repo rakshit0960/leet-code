fe285f97ff0bb527a22939e27ac24c223cb57cb2
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-03-28 21:49:16 +0530

    Time: 26 ms (22.60%) | Memory: 18.3 MB (64.84%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -4,20 +4,20 @@ public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
         vector<bool> visited(n, false);
-        stack<int> stk;
+        queue<int> que;
         
         int count = 0;
         for (int i = 0; i < n; i++) {
             if (!visited[i]) count++;
-            stk.push(i);
-            while (!stk.empty()) {
-                int node = stk.top();
+            que.push(i);
+            while (!que.empty()) {
+                int node = que.front();
                 visited[node] = true;
-                stk.pop();
+                que.pop();
 
                 for (int adj = 0; adj < n; adj++) {
                     if (isConnected[node][adj] == 1 && !visited[adj])
-                        stk.push(adj);
+                        que.push(adj);
                 }
             }
         }
