0e49de42f3b00b4c6caf0cbb1177e8a727614010
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-04-23 06:01:44 -0700

    Time: 103 ms (79.42%) | Memory: 52.8 MB (98.58%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.cpp
+++ 310-minimum-height-trees/minimum-height-trees.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
         vector<int> inOrder(n, 0);
-        vector<int> adj[n];
+        vector<int> adj[n]; 
 
         if (n == 1) return { 0 };
 
+        // fill adjMatrix and inOrder array
         for (auto& edge : edges) {
             int node = edge[0], adjNode = edge[1];
             inOrder[node]++;
@@ -15,12 +16,15 @@ public:
         }
 
         queue<int> leaves;
+
+        // add leaves nodes to the queue
         for (int i = 0; i < n; i++) {
             if (inOrder[i] == 1) {
                 leaves.push(i);
             }    
         }
         
+        // delete leaves nodes and and add new leaves to the queue until n <= 2 node remains
         while (n > 2){
             int size = leaves.size();
             for (int i = 0; i < size; i++) {
@@ -35,6 +39,8 @@ public:
         }
 
         vector<int> res; 
+
+        // add remaining nodes to the result
         while (!leaves.empty()) {
             res.push_back(leaves.front());
             leaves.pop();
