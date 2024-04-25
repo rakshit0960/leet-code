03d130740dd5e9c236e6ae4a637a2b3443f0bb3b
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-04-25 02:53:50 -0700

    Time: 41 ms (84.03%) | Memory: 11.6 MB (62.18%) - LeetSync

---
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestIdealString(string s, int k) {
+        int n = s.size();
+        vector<int> dp(256);
+        
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            dp[ch] = max(1, dp[ch] + 1);
+            for (int j = 1; j <= k; j++) {
+                dp[ch] = max(dp[ch], dp[ch + j] + 1);
+                dp[ch] = max(dp[ch], dp[ch - j] + 1);
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            cout << dp[i + 'a'] << " ";
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
