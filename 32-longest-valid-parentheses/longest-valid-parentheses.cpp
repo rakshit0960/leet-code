5b94a679621c06cb4df6a582f45f4361369544e7
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-05-03 21:54:39 -0700

    Time: 3 ms (72.44%) | Memory: 8.6 MB (68.36%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<pair<char, int>> stk;
-
-        for (int i = 0; i < s.size(); i++) {
-            if (!stk.empty() && s[i] == ')' && stk.top().first =='(')
+        int n = s.size();
+        stack<int> stk;
+        for (int i = 0; i < n; i++) {
+            if (!stk.empty() && s[i] == ')' && s[stk.top()] =='(')
                 stk.pop();
             else 
-                stk.push({s[i], i});
+                stk.push(i);
         }
 
         if (stk.empty()) return s.size();
 
-        int minIdx = INT_MAX, maxIdx = INT_MIN, prev = -1, res = 0;
+        int a = n, b = 0, res = 0;
         while (!stk.empty()) {
-            auto top = stk.top();
-            if (prev == -1) prev = top.second;
-            res = max(res, prev - top.second - 1);
-            prev = top.second;
-            maxIdx = max(maxIdx, prev);
-            minIdx = min(minIdx, prev);
-            stk.pop();
+            b = stk.top(); stk.pop();
+            res = max(res, a-b-1);
+            a = b;
         }
-
-
-        res = max(res, minIdx);
-        res = max(res, int(s.size()) - maxIdx - 1);
+        res = max(res, a);
 
         return res;
     }
