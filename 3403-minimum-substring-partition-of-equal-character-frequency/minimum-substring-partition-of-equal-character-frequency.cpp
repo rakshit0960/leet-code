3d9e1fae1bceb64a41380fb17d314fd030c1d707
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-06-02 23:56:15 +0530

    Time: 74 ms (99.14%) | Memory: 16.9 MB (67.61%) - LeetSync

--- 3403-minimum-substring-partition-of-equal-character-frequency/minimum-substring-partition-of-equal-character-frequency.cpp
+++ 3403-minimum-substring-partition-of-equal-character-frequency/minimum-substring-partition-of-equal-character-frequency.cpp
@@ -1,32 +1,24 @@
 class Solution {
-private:
-    int f(int index, vector<int>& cache, string s) {
-        int n = s.size();
-        if (index >= n) return 0;
-        if (cache[index] != -1) return cache[index];
-        
-        vector<int> freq(26, 0); // frequencies of characters
-        
-        int res = INT_MAX;
-        int maxFreq = 0, uniqueCount = 0;
-        for (int i = index; i < n; i++) {
-            char ch = s[i]; // current char
-            if (freq[ch - 'a'] == 0) uniqueCount++;
-            freq[ch - 'a']++; 
-            int chFreq = freq[ch - 'a']; // frequency of current char 
-            maxFreq = max(maxFreq, chFreq);
-
-            // if max(freq) * unique == n
-            if (maxFreq * uniqueCount == i - index + 1)
-                res = min(res, 1 + f(i + 1, cache, s));
-        }
-        return cache[index] = res;
-    }
 public:
     int minimumSubstringsInPartition(string s) {
         int n = s.size();
-        // unordered_map<int, int> cache;
-        vector<int> cache(n, -1);
-        return f(0, cache, s);
+        vector<int> dp(n + 1, INT_MAX);
+        dp[n] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            vector<int> freq(26, 0);
+            int uniqueChar = 0, maxFreq = 0;
+            for (int j = i; j < n; j++) {
+                char ch = s[j];
+                if (freq[ch - 'a'] == 0) uniqueChar++;
+                freq[ch - 'a']++;
+                maxFreq = max(maxFreq, freq[ch - 'a']);
+
+                if (maxFreq * uniqueChar == j - i + 1)
+                    dp[i] = min(dp[i], 1 + dp[j + 1]);
+            }
+        }
+        for (int i : dp) cout << i << " ";
+        return dp[0];
     }
 };
