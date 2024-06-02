b2cff146056c0881e8d1947e5fd98648e88f9c22
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-06-02 23:45:26 +0530

    Time: 540 ms (67.61%) | Memory: 414.9 MB (9.27%) - LeetSync

--- 3403-minimum-substring-partition-of-equal-character-frequency/minimum-substring-partition-of-equal-character-frequency.cpp
+++ 3403-minimum-substring-partition-of-equal-character-frequency/minimum-substring-partition-of-equal-character-frequency.cpp
@@ -5,18 +5,19 @@ private:
         if (index >= n) return 0;
         if (cache[index] != -1) return cache[index];
         
-        // if max(freq) * unique == n
-        unordered_map<int, int> freq; // frequencies of characters
+        vector<int> freq(26, 0); // frequencies of characters
         
         int res = INT_MAX;
-        int maxFreq = 0;
+        int maxFreq = 0, uniqueCount = 0;
         for (int i = index; i < n; i++) {
             char ch = s[i]; // current char
-            freq[ch]++; 
-            int chFreq = freq[ch]; // frequency of current char 
+            if (freq[ch - 'a'] == 0) uniqueCount++;
+            freq[ch - 'a']++; 
+            int chFreq = freq[ch - 'a']; // frequency of current char 
             maxFreq = max(maxFreq, chFreq);
 
-            if (maxFreq * freq.size() == i - index + 1)
+            // if max(freq) * unique == n
+            if (maxFreq * uniqueCount == i - index + 1)
                 res = min(res, 1 + f(i + 1, cache, s));
         }
         return cache[index] = res;
