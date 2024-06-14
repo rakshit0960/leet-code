408c7afcdc6206b6e5b51f8efe92b5f5348dc5d3
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-06-14 17:52:04 +0530

    Time: 85 ms (15.41%) | Memory: 57.3 MB (93.11%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
@@ -4,14 +4,18 @@ class Solution {
     public int minIncrementForUnique(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
 
+        int maxElement = 0;
         for (int num : nums) {
+            maxElement = Math.max(maxElement, num);
             map.putIfAbsent(num, 0);
             map.put(num, map.get(num) + 1);
         }
-        
+
+        maxElement = Math.max(maxElement, nums.length);
         int res = 0;
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i <= 2 * (int)10e5 ; i++) {
+        for (int i = 0; i <= 2 * maxElement; i++) {
+            if (i > maxElement && stack.empty()) break;
             if (!map.containsKey(i)) {
                 if (stack.isEmpty()) continue;
                 int top = stack.peek();
