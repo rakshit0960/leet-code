941d47906b5d1382d6b53afdcb3a1dde4c8aa4cb
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-06-14 17:58:26 +0530

    Time: 37 ms (72.13%) | Memory: 60.3 MB (31.15%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
@@ -1,31 +1,12 @@
-import java.util.*;
-
 class Solution {
     public int minIncrementForUnique(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Arrays.sort(nums);
 
-        int maxElement = 0;
-        for (int num : nums) {
-            maxElement = Math.max(maxElement, num);
-            map.putIfAbsent(num, 0);
-            map.put(num, map.get(num) + 1);
-        }
-
-        maxElement = Math.max(maxElement, nums.length);
         int res = 0;
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i <= 2 * maxElement; i++) {
-            if (i > maxElement && stack.empty()) break;
-            if (!map.containsKey(i)) {
-                if (stack.isEmpty()) continue;
-                int top = stack.peek();
-                res += i - top;
-                map.put(top, map.get(top) - 1);
-                if (map.get(top) <= 1) stack.pop();
-            }        
-            else if (map.get(i) > 1) {
-                stack.push(i);
-            }
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) continue;
+            res += nums[i - 1] - nums[i] + 1; 
+            nums[i] = nums[i - 1] + 1;
         }
 
         return res;
