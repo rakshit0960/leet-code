ff5061f09a8bb33f92216963dc45e72e43e276df
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-06-24 02:05:59 +0530

    Time: 69 ms (7.37%) | Memory: 51.3 MB (44.95%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.ts
@@ -0,0 +1,19 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    
+    return function (...args) {
+        let res = fn(...args);
+        fn = () => undefined;
+        return res;
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
