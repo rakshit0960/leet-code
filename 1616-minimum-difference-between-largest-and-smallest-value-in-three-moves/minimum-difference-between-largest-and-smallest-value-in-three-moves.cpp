95644e0d14e52907862fd0c4c74644deeb17552d
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-07-04 12:20:06 +0530

    Time: 63 ms (80.62%) | Memory: 41.5 MB (5.06%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -4,13 +4,24 @@ public:
         int n = nums.size();
         if (n <= 3) return 0;
 
-        sort(nums.begin(), nums.end());
+        priority_queue<int, vector<int>, greater<int>> min_pq(nums.begin(), nums.end());
+        priority_queue<int> max_pq(nums.begin(), nums.end());
 
-        int minDiff = INT_MAX;
-        for (int i = 0; i <= 3; i++) {
-            minDiff = min(minDiff, nums[n - 4 + i] - nums[i]);
+        vector<int> min4, max4;
+
+        for (int i = 0; i < 4; i++) {
+            min4.push_back(min_pq.top());
+            min_pq.pop();
+            
+            max4.push_back(max_pq.top());
+            max_pq.pop();
         }
 
+        int minDiff = INT_MAX;
+        for (int i = 0; i < 4; i++) {
+            cout << endl;
+            minDiff = min(minDiff, max4[i] - min4[3 - i]);
+        }
         return minDiff;
     }
 };
