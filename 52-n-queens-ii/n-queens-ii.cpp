a88b5e23f1cd93e8519bf38195dab6f7f624a343
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-08-11 01:14:11 +0530

    Time: 14 ms (14.23%) | Memory: 11.5 MB (15.18%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -1,59 +1,24 @@
 class Solution {
-    bool isValid(vector<int>& board, int row, int index) {
-        int n = board.size();
-
-        // check vertical
-        for (int i = 0; i < n; i++) {
-            if (i == row) continue;
-            if (board[i] & (1<<index)) return false;
-        }
-
-        // check diagoal 
-        int i = row - 1, j = index + 1;
-        while(i >= 0 && j < n) {
-            if (board[i] & (1<<j)) return false;
-            i--;
-            j++;
-        }
-        
-        i = row + 1, j = index - 1;
-        while (i < n && j >= 0) {
-            if (board[i] & (1<<j)) return false;
-            i++;
-            j--;
-        }
-
-        i = row + 1, j = index + 1;
-        while (i < n && j < n) {
-            if (board[i] & (1<<j)) return false;
-            i++;
-            j++;
-        }
-
-        i = row - 1, j = index - 1;
-        while (i >= 0 && j >= 0) {
-            if (board[i] & (1<<j)) return false;
-            i--;
-            j--;
-        }
-
-        return true;
-    }
 
 public:
     int totalNQueens(int n) {
-        vector<int> board(n, 0);
+        unordered_set<int> cols, posDig, negDig;
 
         function<int(int)> dfs;
         dfs = [&] (int row) {
             if (row >= n) return 1;
             
             int res = 0;
-            for (int i = 0; i < n; i++) {
-                if (isValid(board, row, i)) {
-                    board[row] = (1<<i);
+            for (int col = 0; col < n; col++) {
+                if (!cols.count(col) && !posDig.count(row - col) && !negDig.count(row + col)) {
+                    cols.insert(col);
+                    posDig.insert(row - col);
+                    negDig.insert(row + col);
                     res += dfs(row + 1);
-                    board[row] = 0;
+                    cols.erase(col);
+                    posDig.erase(row - col);
+                    negDig.erase(row + col);
+
                 }
             }
 
