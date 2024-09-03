eb1b5b86e4b2a202d56a4740bd66f447492b23e5
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-09-03 19:41:30 +0530

    Time: 3 ms (49.11%) | Memory: 7.8 MB (55.47%) - LeetSync

--- 468-validate-ip-address/validate-ip-address.cpp
+++ 468-validate-ip-address/validate-ip-address.cpp
@@ -1,4 +1,66 @@
 class Solution {
+
+    bool checkIsValidIpv4(string queryIP) {
+        const int n = queryIP.size();
+        int sectionCount = 0;
+        int s = 0;
+        for (int e = 0; e <= n; e++) {
+            if (e != n) {
+                if (queryIP[e] != '.') continue;
+            }
+            // either e == n || e != n And q[e] == '.'
+            if (e == s || e - s > 3) return false;
+
+            string curr = "";
+            for (int i = s; i < e; i++) {
+                // symbols
+                if (queryIP[i] < '0' || queryIP[i] > '9') return false;
+                curr += queryIP[i];
+            }
+            // leading zeros
+            if (curr.size() > 1 && curr[0] == '0') return false;
+            int num = stoi(curr);
+
+            // num out of range
+            if (num < 0 || num > 255) return false;
+            sectionCount++;
+            s = e + 1;
+        }
+        if (sectionCount == 4) return true;
+        return false;
+    }
+
+    bool checkIsValidIpv6(string queryIP) {
+        const int n = queryIP.size();
+        int sectionCount = 0;
+        int s = 0;
+        for (int e = 0; e <= n; e++) {
+            if (e != n) {
+                if (queryIP[e] != ':') continue;
+            }
+            // either e == n || e != n And q[e] == ':'
+
+            if (e == s || e - s > 4) return false;
+
+            for (int i = s; i < e; i++) {
+                // if not a number
+                if (queryIP[i] < '0' || queryIP[i] > '9') {
+                    // if not a small case letter a to f
+                    if (queryIP[i] < 'a' || queryIP[i] > 'f') {
+                        // if not a uppercase letter A to F
+                        if (queryIP[i] < 'A' || queryIP[i] > 'F') {
+                            return false;
+                        }   
+                    }
+                }
+            }
+            sectionCount++;
+            s = e + 1;
+        }
+        if (sectionCount == 8) return true;
+        return false;
+    }
+
 public:
     string validIPAddress(string queryIP) {
         const int n = queryIP.size();
@@ -11,64 +73,18 @@ public:
         if (!hasPeriod && !hasColon) return "Neither";
 
         if (hasPeriod) {
-            int sectionCount = 0;
-            int s = 0;
-            for (int e = 0; e <= n; e++) {
-                if (e != n) {
-                    if (queryIP[e] != '.') continue;
-                }
-                // either e == n || e != n And q[e] == '.'
-                if (e == s || e - s > 3) return "Neither";
-
-                string curr = "";
-                for (int i = s; i < e; i++) {
-                    // symbols
-                    if (queryIP[i] < '0' || queryIP[i] > '9') return "Neither";
-                    curr += queryIP[i];
-                }
-                // leading zeros
-                if (curr.size() > 1 && curr[0] == '0') return "Neither";
-                int num = stoi(curr);
-
-                // num out of range
-                if (num < 0 || num > 255) return "Neither";
-                sectionCount++;
-                s = e + 1;
-            }
-            if (sectionCount == 4) return "IPv4";
+            if (checkIsValidIpv4(queryIP))
+                return "IPv4";
+            else 
+                return "Neither";
         }
         else {
-            int sectionCount = 0;
-            int s = 0;
-            for (int e = 0; e <= n; e++) {
-                if (e != n) {
-                    if (queryIP[e] != ':') continue;
-                }
-
-                if (e == s || e - s > 4) return "Neither";
-
-                // either e == n || e != n And q[e] == ':'
-                string curr = "";
-                for (int i = s; i < e; i++) {
-                    // if not a number
-                    if (queryIP[i] < '0' || queryIP[i] > '9') {
-                        // if not a small case letter a to f
-                        if (queryIP[i] < 'a' || queryIP[i] > 'f') {
-                            // if not a uppercase letter A to F
-                            if (queryIP[i] < 'A' || queryIP[i] > 'F') {
-                                return "Neither";
-                            }   
-                        }
-                    }
-                    curr += queryIP[i];
-                }
-                if (curr.size() > 4) return "Neither";
-                sectionCount++;
-                s = e + 1;
-            }
-            if (sectionCount == 8) return "IPv6";
+            if (checkIsValidIpv6(queryIP)) 
+                return "IPv6";
+            else
+                return "Neither"; 
         }
 
-        return "Neither";
+        return "Neither"; 
     }
 };
