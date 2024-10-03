e8c12e5960170590fa0303eb56931d7a8b023399
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-10-03 14:37:26 +0530

    Time: 59 ms (5.14%) | Memory: 16.5 MB (97.43%) - LeetSync

--- 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.py
+++ 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.py
@@ -1,18 +1,15 @@
 class Solution:
     def kthCharacter(self, k: int, operations: List[int]) -> str:
-        def func(k):
-            if k == 1: return 'a'
-            
-            i = floor(log(k - 1, 2))
-            l = 1 << i
-
-            ch = func(k - l)
-            if operations[i] == 0:
-                return ch
-            else:
-                if ch == 'z':
-                    return 'a'
-                else:
-                    return chr(ord(ch) + 1)
+        if k == 1: return 'a'
         
-        return func(k)
+        i = floor(log(k - 1, 2))
+        l = 1 << i
+
+        ch = self.kthCharacter(k - l, operations)
+        if operations[i] == 0:
+            return ch
+        else:
+            if ch == 'z':
+                return 'a'
+            else:
+                return chr(ord(ch) + 1)
