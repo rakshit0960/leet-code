87c869bd3249af41a7d6a23f80ed08dd93ab1c91
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-10-08 23:40:54 +0530

    Time: 67 ms (38.71%) | Memory: 57.1 MB (16.94%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
-        const int n = skill.size();
-        sort(skill.begin(), skill.end());
+        unordered_map<int, int> mp;
+        long long total = accumulate(skill.begin(), skill.end(), 0ll);
 
-        int i = 0, j = n - 1;
-
-        int sum = skill[0] + skill[n - 1];
+        int sum = total / (skill.size()/2);
         long long res = 0;
-        while (i < j) {
-            if (skill[i] + skill[j] != sum) return - 1;
-            res += skill[i] * skill[j];
-            i++;
-            j--;
+
+        for (int skl : skill) mp[skl]++;
+        
+        for (auto it : mp) {
+            long long num = it.first;
+            int count = it.second;
+            
+            if (mp[sum - num] != count) return -1;
+
+            res += num * (sum - num) * count;
         }
 
-        return res;
+        return res / 2;
+
     }
 };
