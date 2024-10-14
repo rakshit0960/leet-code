58d361997dc277ef7d74bc6936bdb6b7387809ca
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-10-15 01:08:48 +0530

    Time: 4 ms (53.27%) | Memory: 10.9 MB (52.14%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
-        int n = stones.size();
-        const int total = accumulate(stones.begin(), stones.end(), 0ll);
-        
-        int dp[n + 1][total + 1];
-        memset(dp, 0, sizeof(dp));
-        
-        for (int t = 0; t <= total; t++) {
-            dp[n][t] = abs(t - (total - t));
-        }
+        function<int(int, int)> func;
+        const int n = stones.size();
+        const int total = accumulate(stones.begin(), stones.end(), 0);
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int t = total; t >= 0; t--) {
-                if (t + stones[i] <= total)
-                    dp[i][t] = min(dp[i + 1][t + stones[i]], dp[i + 1][t]);
-            }
-        }
+        int dp[n][total / 2 + 1];
+        memset(dp, -1, sizeof(dp));
 
-        return dp[0][0];
+        func = [&](int i, int sum) {
+            if (i == n || sum >= total / 2) 
+                return abs(sum - (total - sum));
+                
+            if (dp[i][sum] != -1)
+                return dp[i][sum];
+
+            return dp[i][sum] = min(func(i + 1, sum), func(i + 1, sum + stones[i]));
+        };
+
+        return func(0, 0);
     }
 };
