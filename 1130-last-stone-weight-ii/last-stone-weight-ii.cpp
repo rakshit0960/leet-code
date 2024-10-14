46b872f77fcd64b38683f8be1cd4d4f1a968dcbc
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-10-15 00:58:51 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (82.88%) - LeetSync

---
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int n = stones.size();
+        const int total = accumulate(stones.begin(), stones.end(), 0ll);
+        
+ 
+
+        int dp[n + 1][total + 1];
+        memset(dp, 0, sizeof(dp));
+        
+        for (int t = 0; t <= total; t++) {
+            dp[n][t] = abs(t - (total - t));
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int t = total; t >= 0; t--) {
+                if (t + stones[i] <= total)
+                    dp[i][t] = min(dp[i + 1][t + stones[i]], dp[i + 1][t]);
+            }
+        }
+
+        return dp[0][0];
+    }
+};
