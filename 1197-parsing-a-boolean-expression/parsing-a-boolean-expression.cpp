429a21411d9b359feab1bdd71f9be45a35757ca7
Rakshit Mehta <114852204+rakshit0960@users.noreply.github.com>
2024-10-20 19:54:51 +0530

    Time: 139 ms (5.21%) | Memory: 9.6 MB (81.98%) - LeetSync

--- 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -1,38 +1,46 @@
 class Solution {
 public:
-    bool parseBoolExpr(string expression) {
-        stack<char> st;
+    bool parseBoolExpr(string exp) {
+        const int n  = exp.size();
+        function<char(int, int)> func;
 
-        for (char ch : expression) {
-            if (ch != ')') {
-                st.push(ch);
-                continue;
-            }
+        func = [&] (int s, int e) {
+          char op = exp[s];
 
-            int t_count = 0, f_count = 0;
-            while (st.top() != '(') {
-                if (st.top() == 't') t_count++;
-                else if (st.top() == 'f') f_count++;
-                st.pop();
+          int t_count = 0, f_count = 0;
+          for (int i = s + 2; i < e; i++) {
+            if (exp[i] == 't') t_count++;
+            else if (exp[i] == 'f') f_count++;
+            else if (exp[i] == '&' || exp[i] == '|' || exp[i] == '!') {
+                int b_cnt = 1;
+                int j = i + 2;
+                while (b_cnt) {
+                    if (exp[j] == '(') b_cnt++;
+                    if (exp[j] == ')') b_cnt--;
+                    j++;
+                }
+                char ch = func(i, j - 1);
+                cout << i << " " << j - 1 << " " << ch << endl;
+                if (ch == 't') t_count++;
+                else if (ch == 'f') f_count++;
+                i = j - 1;
             }
-            st.pop();
+          }
 
-            if (st.top() == '&') {
-                st.pop();
-                if (f_count) st.push('f');
-                else st.push('t');
-            }
-            else if (st.top() == '|') {
-                st.pop();
-                if (t_count) st.push('t');
-                else st.push('f');
-            } else if (st.top() == '!') { // !
-                st.pop();
-                if (t_count) st.push('f');
-                else st.push('t');
-            }
-        }
-        cout << st.top();
-        return st.top() == 't';
+          if (op == '&') {
+            if (f_count) return 'f';
+            return 't';
+          } else if (op == '|') {
+            if (t_count) return 't';
+            return 'f';
+          } else if (op == '!') {
+            if (t_count) return 'f';
+            return 't';
+          }
+
+          throw 'wrong';
+        };
+
+        return func(0, n - 1) == 't';
     }
 };
